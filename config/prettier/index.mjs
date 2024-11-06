5e8e530eeffb49849a1ca554eecee9c7c4ff2088
Diego Masin <diegoifce@gmail.com>
2024-11-06 18:29:12 -0300

    Ajustes no prettier

--- config/prettier/index.mjs
+++ config/prettier/index.mjs
@@ -1,16 +1,16 @@
-/** @typedef {import("prettier").Config} PrettierConfig  */
+/** @typedef {import('prettier').Config} PrettierConfig */
 
-/** @type {PrettierConfig} */
+/** @type { PrettierConfig } */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
   printWidth: 80,
   tabWidth: 2,
-  useTabs: true,
+  useTabs: false,
   semi: false,
   singleQuote: true,
   quoteProps: "as-needed",
   jsxSingleQuote: false,
-  trailingComma: "es5",
+  trailingComma: "all",
   bracketSpacing: true,
   arrowParens: "always",
   endOfLine: "auto",
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,19 +1,19 @@
 import {
-	AbilityBuilder,
-	CreateAbility,
-	createMongoAbility,
-	ForcedSubject,
-	MongoAbility,
+  AbilityBuilder,
+  CreateAbility,
+  createMongoAbility,
+  ForcedSubject,
+  MongoAbility,
 } from '@casl/ability'
 
 const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 type AppAbilities = [
-	(typeof actions)[number],
-	(
-		| (typeof subjects)[number]
-		| ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-	),
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
 ]
 
 export type AppAbility = MongoAbility<AppAbilities>
